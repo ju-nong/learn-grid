6614ee50558945c49674165f2b2cba84504d6678
이준용 <iiiuooyoo@gmail.com>
2024-01-08 17:41:07 +0900

    update : 공식문서와 블로그 보면서 실습 중
    어려운 레이아웃으로 실습 해보면 금방 기억할 거 같음

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,38 @@
 <template>
-    <div>안녕하세요</div>
+    <div class="container">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
+            laudantium corporis dolore, magni aliquam sed voluptatum eius quae,
+            iure hic beatae, fugiat dolor natus minus ad dignissimos doloribus
+            laboriosam incidunt.
+        </div>
+        <div>5</div>
+        <div>6</div>
+        <div>7</div>
+        <div>8</div>
+        <div>9</div>
+    </div>
 </template>
 
 <script setup lang="ts"></script>
 
-<style scoped></style>
+<style scoped>
+.container {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(20%, auto));
+    /* grid-template-rows: repeat(3, minmax(100px, auto)); */
+    /* grid-template-columns: repeat(auto-fit, minmax(200px, 1fr)); */
+    /* grid-template-columns: repeat(3, 1fr); */
+    /* grid-template-rows: minmax(100px, auto); */
+    /* grid-gap: 20px; */
+
+    > div {
+        padding: 8px;
+        border: 1px solid #999;
+        border-radius: 0.5rem;
+    }
+}
+</style>
